251827e21300eb84c714703fa06f38a49a26ce08
chase-manning <chase@manning.dev>
2022-12-02 10:04:37 +0000

    :art: minor code formatting improvements

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -16,33 +16,33 @@ fn main() {
         let mut iter = line.split_whitespace();
         let them = iter.next().unwrap();
         let me = iter.next().unwrap();
-        match me {
+        total_score += match me {
             "X" => {
-                total_score += 0 + match { them } {
+                0 + match { them } {
                     "A" => 3,
                     "B" => 1,
                     "C" => 2,
                     _ => panic!("Invalid input"),
-                };
+                }
             }
             "Y" => {
-                total_score += 3 + match them {
+                3 + match them {
                     "A" => 1,
                     "B" => 2,
                     "C" => 3,
                     _ => panic!("Invalid input"),
-                };
+                }
             }
             "Z" => {
-                total_score += 6 + match them {
+                6 + match them {
                     "A" => 2,
                     "B" => 3,
                     "C" => 1,
                     _ => panic!("Invalid input"),
-                };
+                }
             }
             _ => panic!("Invalid me"),
-        }
+        };
     }
     println!("{}", total_score);
 }
