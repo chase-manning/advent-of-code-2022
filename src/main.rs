802a32c5e82f17e8efc1014244188f2c1f5ccd07
chase-manning <chase@manning.dev>
2022-12-03 11:30:28 +0000

    :rocket: add cli tool for getting solutions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 mod solutions;
 pub mod utils;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    day: u8,
+    challenge: u8,
+}
+
 fn main() {
-    println!("{}", solutions::day_2_2::solve());
-    println!("{}", solutions::day_1_2::solve());
+    let args = Args::parse();
+
+    println!(
+        "{}",
+        match args.day {
+            1 => match args.challenge {
+                1 => solutions::day_1_1::solve(),
+                2 => solutions::day_1_2::solve(),
+                _ => panic!("Invalid challenge"),
+            },
+            2 => match args.challenge {
+                1 => solutions::day_2_1::solve(),
+                2 => solutions::day_2_2::solve(),
+                _ => panic!("Invalid challenge"),
+            },
+            _ => panic!("Invalid day"),
+        }
+    );
 }
