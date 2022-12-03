0999d6a8bbc72cda299b1044d4788bce52356042
chase-manning <chase@manning.dev>
2022-12-03 11:23:23 +0000

    :rocket: add day 1 1 solution

---
+++ src/solutions/day_1_1.rs
@@ -0,0 +1,25 @@
+use crate::utils::files::get_data_as_lines;
+
+pub fn solve() -> i32 {
+    let lines = get_data_as_lines("day_1_calories.txt");
+
+    let mut highest = 0;
+    let mut elf_calories = 0;
+    for line in lines {
+        if line == "" {
+            if elf_calories > highest {
+                highest = elf_calories;
+            }
+            elf_calories = 0;
+            continue;
+        }
+        let calories = line.parse::<i32>().unwrap();
+        elf_calories += calories;
+    }
+    highest
+}
+
+#[test]
+fn result() {
+    assert_eq!(solve(), 71124);
+}
