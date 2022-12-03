474794754cfd5bbfabffc9d69a55a5f00090afa3
chase-manning <chase@manning.dev>
2022-12-03 11:23:34 +0000

    :rocket: add day 1 1 solution

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,2 +1,4 @@
+pub mod day_1_1;
 pub mod day_1_2;
+pub mod day_2_1;
 pub mod day_2_2;
---
+++ src/solutions/day_2_1.rs
@@ -0,0 +1,45 @@
+use crate::utils::files::get_data_as_lines;
+
+pub fn solve() -> i32 {
+    let lines = get_data_as_lines("day_2_strategy.txt");
+
+    let mut total_score = 0;
+    for line in lines {
+        let mut iter = line.split_whitespace();
+        let them = iter.next().unwrap();
+        let me = iter.next().unwrap();
+        total_score += match me {
+            "X" => {
+                1 + match { them } {
+                    "A" => 3,
+                    "B" => 0,
+                    "C" => 6,
+                    _ => panic!("Invalid input"),
+                }
+            }
+            "Y" => {
+                2 + match them {
+                    "A" => 6,
+                    "B" => 3,
+                    "C" => 0,
+                    _ => panic!("Invalid input"),
+                }
+            }
+            "Z" => {
+                3 + match them {
+                    "A" => 0,
+                    "B" => 6,
+                    "C" => 3,
+                    _ => panic!("Invalid input"),
+                }
+            }
+            _ => panic!("Invalid me"),
+        };
+    }
+    total_score
+}
+
+#[test]
+fn result() {
+    assert_eq!(solve(), 9759);
+}
