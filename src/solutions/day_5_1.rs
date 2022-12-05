5c510c04c74505e22a79e653f1b0047a8e221523
chase-manning <chase@manning.dev>
2022-12-05 11:28:18 +0000

    :art: minor code improvements

--- src/solutions/day_5_1.rs
+++ src/solutions/day_5_1.rs
@@ -1,5 +1,6 @@
 use crate::utils::files::get_data_as_lines;
 
+#[derive(Debug)]
 struct Instruction {
     from: usize,
     to: usize,
@@ -8,55 +9,47 @@ struct Instruction {
 
 pub fn solve() -> String {
     let mut lines = get_data_as_lines("day_5_crates.txt");
-    lines.reverse();
 
-    let mut crates: Vec<Vec<char>> = Vec::new();
+    // Populating data
+    let mut crates: Vec<Vec<char>> = (0..9).into_iter().map(|_| Vec::new()).collect();
     let mut instructions: Vec<Instruction> = Vec::new();
-
-    let mut processing_crates = false;
-    for line in lines {
-        if processing_crates {
+    lines.reverse();
+    lines.iter().for_each(|line| {
+        // Handling crates
+        if line.contains("[") {
             let chars: Vec<char> = line.chars().collect();
-            if chars[1] == '1' {
-                continue;
-            }
-            for i in 0..9 {
-                let index = i * 4 + 1;
-                if chars[index] != ' ' {
-                    if crates.len() <= i {
-                        crates.push(Vec::new());
-                    }
-                    crates[i].push(chars[index]);
+            (0..9).into_iter().for_each(|i| {
+                let char = chars[i * 4 + 1];
+                if char != ' ' {
+                    crates[i].push(char);
                 }
-            }
-        } else {
-            if line == "" {
-                processing_crates = true;
-                continue;
-            }
+            });
+        }
+
+        // Handling instructions
+        if line.contains("move") {
             let mut split = line.split(' ');
-            let amount = split.nth(1).unwrap().parse::<usize>().unwrap();
-            let from = split.nth(1).unwrap().parse::<usize>().unwrap() - 1;
-            let to = split.nth(1).unwrap().parse::<usize>().unwrap() - 1;
-            instructions.push(Instruction { from, to, amount });
+            instructions.push(Instruction {
+                amount: split.nth(1).unwrap().parse::<usize>().unwrap(),
+                from: split.nth(1).unwrap().parse::<usize>().unwrap() - 1,
+                to: split.nth(1).unwrap().parse::<usize>().unwrap() - 1,
+            });
         }
-    }
+    });
 
+    // Moving crates
     instructions.reverse();
-    for instruction in instructions {
-        let from = instruction.from;
-        let to = instruction.to;
-        let mut amount = instruction.amount;
-        while amount > 0 {
-            let crate_from = crates[from].pop().unwrap();
-            crates[to].push(crate_from);
-            amount -= 1;
-        }
-    }
+    instructions.iter().for_each(|instruction| {
+        (0..instruction.amount).into_iter().for_each(|_| {
+            let crate_from = crates[instruction.from].pop().unwrap();
+            crates[instruction.to].push(crate_from);
+        });
+    });
 
+    // Returning result
     crates
         .iter()
-        .map(|c| c[c.len() - 1].to_string())
+        .map(|c| c.last().unwrap().to_string())
         .collect::<Vec<String>>()
         .join("")
 }
--- src/solutions/day_5_2.rs
+++ src/solutions/day_5_2.rs
@@ -8,60 +8,48 @@ struct Instruction {
 
 pub fn solve() -> String {
     let mut lines = get_data_as_lines("day_5_crates.txt");
-    lines.reverse();
 
-    let mut crates: Vec<Vec<char>> = Vec::new();
+    // Populating data
+    let mut crates: Vec<Vec<char>> = (0..9).into_iter().map(|_| Vec::new()).collect();
     let mut instructions: Vec<Instruction> = Vec::new();
-
-    let mut processing_crates = false;
-    for line in lines {
-        if processing_crates {
+    lines.reverse();
+    lines.iter().for_each(|line| {
+        // Handling crates
+        if line.contains("[") {
             let chars: Vec<char> = line.chars().collect();
-            if chars[1] == '1' {
-                continue;
-            }
-            for i in 0..9 {
-                let index = i * 4 + 1;
-                if chars[index] != ' ' {
-                    if crates.len() <= i {
-                        crates.push(Vec::new());
-                    }
-                    crates[i].push(chars[index]);
+            (0..9).into_iter().for_each(|i| {
+                let char = chars[i * 4 + 1];
+                if char != ' ' {
+                    crates[i].push(char);
                 }
-            }
-        } else {
-            if line == "" {
-                processing_crates = true;
-                continue;
-            }
+            });
+        }
+
+        // Handling instructions
+        if line.contains("move") {
             let mut split = line.split(' ');
-            let amount = split.nth(1).unwrap().parse::<usize>().unwrap();
-            let from = split.nth(1).unwrap().parse::<usize>().unwrap() - 1;
-            let to = split.nth(1).unwrap().parse::<usize>().unwrap() - 1;
-            instructions.push(Instruction { from, to, amount });
+            instructions.push(Instruction {
+                amount: split.nth(1).unwrap().parse::<usize>().unwrap(),
+                from: split.nth(1).unwrap().parse::<usize>().unwrap() - 1,
+                to: split.nth(1).unwrap().parse::<usize>().unwrap() - 1,
+            });
         }
-    }
+    });
 
+    // Moving crates
     instructions.reverse();
-    for instruction in instructions {
-        let from = instruction.from;
-        let to = instruction.to;
-        let mut amount = instruction.amount;
-        let mut new_list = Vec::new();
-        while amount > 0 {
-            let crate_from = crates[from].pop().unwrap();
-            new_list.push(crate_from);
-            amount -= 1;
-        }
-        new_list.reverse();
-        for c in new_list {
-            crates[to].push(c);
-        }
-    }
+    instructions.iter().for_each(|instruction| {
+        let mut meow: Vec<char> = (0..instruction.amount)
+            .map(|_| crates[instruction.from].pop().unwrap())
+            .collect();
+        meow.reverse();
+        crates[instruction.to].append(&mut meow);
+    });
 
+    // Returning result
     crates
         .iter()
-        .map(|c| c[c.len() - 1].to_string())
+        .map(|c| c.last().unwrap().to_string())
         .collect::<Vec<String>>()
         .join("")
 }
