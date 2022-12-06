a99028bf0c3978191f04c3ab80e9c543235f7ff9
chase-manning <chase@manning.dev>
2022-12-06 09:51:36 +0000

    :rocket: add solution for day 6

---
+++ src/solutions/day_6_1.rs
@@ -0,0 +1,24 @@
+use crate::utils::files::get_data_as_lines;
+
+fn all_chars_are_different(chars: &Vec<char>) -> bool {
+    let mut chars = chars.clone();
+    chars.sort();
+    chars.dedup();
+    chars.len() == chars.capacity()
+}
+
+pub fn solve() -> String {
+    let lines = get_data_as_lines("day_6_packet.txt");
+    let chars = &lines[0].chars().collect::<Vec<char>>();
+    for i in 0..chars.len() {
+        if all_chars_are_different(&chars[i..i + 4].to_vec()) {
+            return (i + 4).to_string();
+        }
+    }
+    panic!("No solution found");
+}
+
+#[test]
+fn result() {
+    assert_eq!(solve(), "1707");
+}
