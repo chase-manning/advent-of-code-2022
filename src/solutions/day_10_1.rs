febbef41139ee0ddfd7af24ceee64590c713efa7
chase-manning <chase@manning.dev>
2022-12-10 11:48:27 +0000

    :rocket: add day 10 part 1 solution

---
+++ src/solutions/day_10_1.rs
@@ -0,0 +1,31 @@
+use crate::utils::files::get_data_as_lines;
+
+pub fn solve() -> String {
+    let lines = get_data_as_lines("day_10_commands.txt");
+    let cycles = [20, 60, 100, 140, 180, 220];
+
+    let mut cycle = 1;
+    let mut x = 1;
+    let mut strength = 0;
+    for line in lines {
+        if cycles.contains(&cycle) {
+            strength += x * cycle;
+        }
+        cycle += 1;
+        if line != "noop" {
+            let increment = line.split(" ").last().unwrap().parse::<i64>().unwrap();
+            if cycles.contains(&cycle) {
+                strength += x * cycle;
+            }
+            cycle += 1;
+            x += increment;
+        }
+    }
+
+    strength.to_string()
+}
+
+#[test]
+fn result() {
+    assert_eq!(solve(), "14860");
+}
