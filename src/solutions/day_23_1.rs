5043ccd33044e784a973d877996e8b65ae86e537
chase-manning <chase@manning.dev>
2022-12-23 13:34:59 +0000

    :rotating_light: fix lint warnings

--- src/solutions/day_23_1.rs
+++ src/solutions/day_23_1.rs
@@ -15,30 +15,7 @@ fn get_elves(lines: Vec<String>) -> Vec<(isize, isize)> {
     elves
 }
 
-fn print_grove(elves: &Vec<(isize, isize)>) {
-    let min_x = elves.iter().map(|e| e.0).min().unwrap();
-    let max_x = elves.iter().map(|e| e.0).max().unwrap();
-    let min_y = elves.iter().map(|e| e.1).min().unwrap();
-    let max_y = elves.iter().map(|e| e.1).max().unwrap();
-
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            if elves.contains(&(x, y)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
-    println!();
-}
-
-fn can_propose_direction(
-    elves: &Vec<(isize, isize)>,
-    elf: &(isize, isize),
-    direction: &char,
-) -> bool {
+fn can_propose_direction(elves: &[(isize, isize)], elf: &(isize, isize), direction: &char) -> bool {
     let (a, b, c) = match direction {
         'N' => (
             (elf.0, elf.1 - 1),
@@ -75,33 +52,33 @@ fn get_position_after_move(elf: &(isize, isize), direction: &char) -> (isize, is
     }
 }
 
-fn is_alone(elves: &Vec<(isize, isize)>, elf: &(isize, isize)) -> bool {
+fn is_alone(elves: &[(isize, isize)], elf: &(isize, isize)) -> bool {
     !elves
         .iter()
         .any(|e| e != elf && (e.0 - elf.0).abs() <= 1 && (e.1 - elf.1).abs() <= 1)
 }
 
-fn move_elves(elves: &mut Vec<(isize, isize)>) {
+fn move_elves(elves: &mut [(isize, isize)]) {
     let directions = vec!['N', 'S', 'W', 'E'];
     for i in 0..10 {
         let mut elf_proposals: HashMap<(isize, isize), (isize, isize)> = HashMap::new();
         let mut proposed_count: HashMap<(isize, isize), usize> = HashMap::new();
         for elf in elves.iter() {
-            if is_alone(&elves, &elf) {
+            if is_alone(elves, elf) {
                 continue;
             }
             for j in 0..4 {
                 let direction = directions[(i + j) % 4];
-                if can_propose_direction(elves, &elf, &direction) {
-                    let pos = get_position_after_move(&elf, &direction);
-                    elf_proposals.insert(elf.clone(), pos);
+                if can_propose_direction(elves, elf, &direction) {
+                    let pos = get_position_after_move(elf, &direction);
+                    elf_proposals.insert(*elf, pos);
                     proposed_count.insert(pos, proposed_count.get(&pos).unwrap_or(&0) + 1);
                     break;
                 }
             }
         }
         for (elf, position) in &elf_proposals {
-            if proposed_count.get(&position).unwrap() == &1 {
+            if proposed_count.get(position).unwrap() == &1 {
                 let index = elves.iter().position(|e| e == elf).unwrap();
                 elves[index] = *position;
             }
